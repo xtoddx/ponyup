1052680115149eef1b995c7b7d1ab1d9d66c1ace
Todd Willey <xtoddx@gmail.com>
2014-01-30 16:27:53 -0500

    refactor security groups to couple less to rake.

--- lib/ponyup/components/security.rb
+++ lib/ponyup/components/security.rb
@@ -7,36 +7,60 @@ module Ponyup
     # :nodoc:
     #
     class Security
-      extend Rake::DSL
-
-      # Return the namespace as string
-      def self.define name, public_ports, group_ports
-        namespace :security do
-          namespace name do
-            desc "Create #{name} security group"
-            task :create do
-              SecurityRecord.create name, public_ports, group_ports
-            end
-
-            desc "Delete #{name} security group"
-            task :destroy do
-              SecurityRecord.destroy name
-            end
-          end
-        end
-        "security:#{name}"
+      def initialize name, public_ports=[], group_port_hash={}
+        @name = name
+        @public_ports = Array(public_ports)
+        @group_ports = group_port_hash
       end
 
-      def self.create name, public_ports, group_ports
-        public_ports = Array(public_ports)
+      def create
         group = Fog::Compute[:aws].security_groups.get(name)
         if group
-          delete_all_rules(group)
+          converge_existing_resource(group)
         else
-          group = Fog::Compute[:aws].security_groups.new(name: name,
-                                            description: "Autmated Group #{name}")
-          group.save
+          create_new_resource
         end
+      end
+
+      def destroy
+        if group=Fog::Compute[:aws].security_groups.get(resource_name)
+          group.delete
+        end
+      end
+
+      private
+
+      def resource_name
+        "#{@name}#{Ponyup.resource_suffix}"
+      end
+
+      def create_new_resoruce
+        group = Fog::Compute[:aws].security_groups.new(name: name,
+                                          description: "#{name} [auto]")
+        group.save
+        add_ports_to_group group
+      end
+
+      def converge_existing_resource group
+        delete_all_rules group
+        add_ports_to_group group
+      end
+
+      def delete_all_rules group
+        group.ip_permissions.each do |perm|
+          ports = (perm['fromPort'] .. perm['toPort'])
+          if perm['groups'].any?
+            perm['groups'].each do |g|
+              group_spec = {g['userId'] => g['groupId']}
+              group.revoke_port_range(ports, group: group_spec)
+            end
+          else
+            group.revoke_port_range(ports)
+          end
+        end
+      end
+
+      def add_ports_to_group group
         unless public_ports.empty?
           add_public_ports(group, public_ports)
         end
@@ -48,11 +72,32 @@ module Ponyup
         end
       end
 
-      def self.destroy name, ports
-        if group=Fog::Compute[:aws].security_groups.get(name)
-          group.delete
+      # JUNKY OLD RAKE STUFF
+      public
+
+      extend Rake::DSL
+
+      # Return the namespace as string
+      def self.define name, public_ports, group_ports
+        instance = new(name, public_ports, group_ports)
+        namespace :security do
+          namespace name do
+            instance_task "Create #{name} security group",
+                          create: instance.method(:create)
+            instance_task "Delete #{name} security group",
+                          destroy: instance.method(:destroy)
+          end
         end
+        "security:#{name}"
       end
+
+      private
+
+      def self.instance_task description, named_method
+        desc description
+        task named_method.keys.first, &named_method.values.first
+      end
+
 
       def self.add_public_ports group, ports
         ports.each do |range|
@@ -70,19 +115,6 @@ module Ponyup
         end
       end
 
-      def self.delete_all_rules group
-        group.ip_permissions.each do |perm|
-          ports = (perm['fromPort'] .. perm['toPort'])
-          if perm['groups'].any?
-            perm['groups'].each do |g|
-              group_spec = {g['userId'] => g['groupId']}
-              group.revoke_port_range(ports, group: group_spec)
-            end
-          else
-            group.revoke_port_range(ports)
-          end
-        end
-      end
     end
   end
 end
